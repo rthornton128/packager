5a153922709a37ac015f6f201d211c73bbeb4fc6
Rob <rthornton@acromediainc.com>
2017-02-07 11:52:45 -0800

    fix many, many bugs

--- packager.sh
+++ packager.sh
@@ -6,35 +6,37 @@ BINDIR="$INSTALLDIR/bin"
 WEBROOT="/var/www"
 
 # Package lists
-WEBSRV=$(cat "websrv.list")
-PHP=$(cat "php.list")
-DB=$(cat "db.list")
-EXTRA=$(cat "drupal.list") $(cat "extra.list")
+WEBSRV=`cat websrv.list`
+PHP=`cat php.list`
+DB=`cat db.list`
+EXTRA=`cat drupal.list`
+EXTRA="${EXTRA} `cat extra.list`"
 
 # Colors
 YLW="\033[0;33m"
 GRY="\033[0;37m"
 
+# check if program is installed and exit if not with error message
+require_prog() {
+	eval $1=`which $2`
+	if [ ! $1 ]; then
+		echo "$2 not installed. Use 'apt-get install $2' and run this program again";
+		exit 1
+	fi
+}
+
 # Composer is a handy way to install Drupal. The fastest for a fairly bare-bones install
 # is using: '
 composer_install() {
-	WGET=$(which "wget")
-	echo "Output=$WGET"
-	if [ ! $WGET ]; then
-		echo "wget not installed. Use 'apt-get install wget' and run this program again";
-		return 1
-	fi
-
-	PHP=$(which "php")
-	if [ ! $PHP ]; then
-		echo "php not installed. Use 'apt-get install php' and run this program again";
-	fi
+	require_prog WGET wget
+	require_prog PHP php
 
 	echo "Installing composer..."
 	FILENAME="composer-setup.php"
 	$WGET -vO "$FILENAME" "http://getcomposer.org/installer"
 	$PHP "$FILENAME" --install-dir="/usr/local/bin" --filename="composer"
-	chmod +x "/usr/local/bin/composer"
+	chmod 775 "/usr/local/bin/composer"
+	chown $USER:$USER /home/$USER/.composer
 	rm -f "$FILENAME"
 
 	echo -n "${YLW}INFO${GRY}: To install Drupal, run:"
@@ -46,7 +48,9 @@ composer_install() {
 
 composer_install_extra() {
 	# install the following php packages globally but not as root per composer warning
-	USER=$(logname)
+	if [ ! -z $SUDO_USER ]; then
+		USER=$SUDO_USER
+	fi
 	COMPOSER_HOME="/home/$USER/.composer"
 	COMPOSER_VENDOR="$COMPOSER_HOME/vendor"
 	sudo -u $USER composer global require drupal/coder
@@ -60,20 +64,29 @@ $COMPOSER_VENDOR/drupal/coder/coder_sniffer
 # fail
 nginx_config() {
 	echo "Setting $WEBROOT ownership to root:www-data and adding $SUDO_USER to www-data"
-	chown "root:www-data $WEBROOT"
-	usermod -a -G "www-data" $SUDO_USER
+	#chown root:www-data $WEBROOT
+	#usermod -a -G "www-data" $SUDO_USER
 }
 
 # On ubuntu, nodejs is installed as "nodejs" but gulp/sass will fail to install because
 # their install scripts expect to find "node". This will create a symlink if node does not
 # exist
 nodejs_config() {
-	NODEJS=$(which "nodejs")
-	if [ -f "/usr/bin/node" ] && [ ! $(which "node") ]; then
+	require_prog NODEJS nodejs
+	if [ ! -e "/usr/bin/node" ] && [ ! -e "/usr/local/bin/node" ] && [ ! $(which "node") ]; then
 		echo "'node' not found, creating symlink";
-		ln -s $NODEJS "/usr/bin/node";
+		ln -s $NODEJS "/usr/local/bin/node";
+	else
+		echo "'node' installed properly";
 	fi
-	echo "'node' installed properly"
+}
+
+nodejs_extra() {
+	require_prog NPM npm
+	$NPM install --global gulp-cli
+	$NPM install --global gulp
+	$NPM install --global gulp-sass
+	$NPM install --global gulp-autoprefixer
 }
 
 package_install() {
@@ -81,37 +94,27 @@ package_install() {
 	echo "apt udate"
 	apt-get update
 	echo "installing packages";
-	apt-get install $WEBSRV $PHP $DB $EXTRA
+	apt-get install $WEBSRV $DB $PHP $EXTRA
 }
 
 php_config() {
-	GREP=$(which "grep")
-	if [ ! $GREP ]; then
-		echo "grep not installed. Use 'apt-get install grep' and run this program again";
-		return 1
-	fi
-
-	SED=$(which "sed")
-	if [ ! $SED ]; then
-		echo "sed not installed. Use 'apt-get install sed' and run this program again";
-		return 1
-	fi
+	require_prog GREP grep
+	require_prog SED sed
 
 	echo "Configuring PHP"
-	if [ ! -f "/etc/php/7.0/fpm/php.ini" ]; then
-		echo "couldn't find php.ini"
-		return 2
-	fi
-
 	PHPINI="/etc/php/7.0/fpm/php.ini"
+	if [ ! -f "$PHPINI" ]; then
+		echo "couldn't find php.ini"
+		exit 2
+	fi
 
 	#backup php.ini
 	echo "..backing up php.ini"
-	cp -f $PHPINI $PHPINI.bak
+	cp -v $PHPINI $PHPINI.bak
 	$SED -i "s/max_execution_time \= [0-9]*$/max_execution_time \= 300/" $PHPINI
 	$SED -i "s/memory_limit \= [0-9]*M$/memory_limit \= 128M/" $PHPINI
-	echo "..set: $($GREP 'max_execution_time =')"
-	echo "..set: $($GREP 'memory_limit =')"
+	echo "..set: $($GREP 'max_execution_time =' $PHPINI)"
+	echo "..set: $($GREP 'memory_limit =' $PHPINI)"
 
 	XDEBUG="/etc/php/7.0/fpm/conf.d/20-xdebug.ini" 
 	if [ -f $XDEBUG ]; then
@@ -134,15 +137,16 @@ case $1 in
 	"install") package_install ;;
 	"composer") composer_install;;
 	"composer-extra") composer_install_extra ;;
-	"nodejs") nodejs_config;;
+	"node") nodejs_config;;
+	"node-extra") nodejs_extra;;
 	"php") php_config ;;
 	"nginx") nginx_config ;;
 	"all")
 		package_install # MUST go first
 		nginx_config # must come after package_install
-		php_config
+		php_config # keep this third
 		composer_install; composer_install_extra
-		nodejs_config
+		nodejs_config; nodejs-extra
 	;;
 	*) echo 'enter a command to execute or "all"' ;;
 esac
